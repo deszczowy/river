a513145dee769c2637a7274ec283c2f2306f6e94
deszczowy <krystian.szklarek@op.pl>
2023-12-04 23:30:08 +0100

    note generation revisited

--- engine/Note.py
+++ engine/Note.py
@@ -1,4 +1,10 @@
 from engine.Functions import *
+from enum import Enum
+
+class EnProcessMode(Enum):
+    Normal = 0
+    Tag = 1
+    Notice = 2
 
 class Note:
 
@@ -12,53 +18,98 @@ class Note:
         self.note = ""
         self.tags = []
         self.notices = []
-        self.work(txt)
 
-    def work(self, txt):
-        current = ""
-        tag = ""
-        notice = ""
+        self.proceed(txt)
 
-        self.tags = []
-        inTag = False
-        inNotice = False
+    def setup(self):
+        # temps
+        self.current = ""
+        self.tag = ""
+        self.notice = ""
+        self.mode = EnProcessMode.Normal
 
-        for character in txt:
-            if character != "#":
-                if character == ":":
-                    self.title = current
-                    current = ""
-                elif character == "[":
-                    inNotice = True
-                elif character == "]":
-                    if notice != "":
-                        self.notices.append(notice)
-                        notice = ""
-                    inNotice = False
-                elif character == "@":
-                    inTag = True
-                    current += character
-                elif character in self.stoppers:
-                    current += character
-                    if tag != "":
-                        self.tags.append(tag)
-                        tag = ""
-                    inTag = False
-                else:
-                    if inTag:
-                        tag += character
-                        current += character
-                    elif inNotice:
-                        notice += character
-                    else:
-                        current += character
+    def proceed(self, text):
+        self.setup()
+
+        for c in text:
+            if c == "#":
+                continue
+            
+            if c in self.stoppers:
+                self.pause(c)
+            elif c == "@":
+                self.start_tag()
+            elif c == ":":
+                self.retrieve_title()
+            elif c == "[":
+                self.start_notice()
+            elif c == "]":
+                self.close_notice()
             else:
-                break
-        if tag != "":
-            self.tags.append(tag)
-        if notice != "":
-            self.notices.append(notice)
+                self.store_char(c)
 
-        self.note = current
+        self.close_note()
+        #self.print_me()
+    
+    def pause(self, char):
+        if self.mode == EnProcessMode.Notice:
+            self.notice += char
+        else:
+            if self.mode == EnProcessMode.Tag:
+                self.close_tag()
+            self.current += char
+
+    def close_tag(self):
+        if self.tag != "":
+            self.tags.append(self.tag)
+            self.tag = ""
+        self.mode = EnProcessMode.Normal
+
+    def start_tag(self):
+        if self.mode == EnProcessMode.Notice:
+            self.notice += "@"
+        else:
+            if self.mode == EnProcessMode.Tag:
+                self.close_tag()
+            self.mode = EnProcessMode.Tag
+            self.current += "@"
+
+    def retrieve_title(self):
+        self.title = self.current
+        self.current = ""
+    
+    def start_notice(self):
+        self.mode = EnProcessMode.Notice
+    
+    def close_notice(self):
+        self.mode = EnProcessMode.Normal
+        if self.notice != "":
+            self.notices.append(self.notice)
+        self.notice = ""
+
+    def close_note(self):
+        self.close_tag()
+        self.close_notice()
+        
+        self.note = self.current
         if self.title == "":
             self.title = self.note[:20] + "..."
+    
+    def store_char(self, char):
+        if self.mode == EnProcessMode.Notice:
+            self.notice += char
+        else:
+            if self.mode == EnProcessMode.Tag:
+                self.tag += char
+            self.current += char
+    
+    def print_me(self):
+        print(":: {}".format(self.id))
+        print("Title: {}".format(self.title))
+        print("Content: {}".format(self.note))
+        print("Tags: ")
+        for t in self.tags:
+            print("   {}".format(t))
+        print("Notices: ")
+        for n in self.notices:
+            print(" * {}".format(n))
